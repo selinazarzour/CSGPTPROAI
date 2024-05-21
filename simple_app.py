36995d1b342d18c644df7969707100eb7052aab2
Selina Zarzour <selinazarzour@selinas-mbp-3.home>
2024-05-21 00:10:48 -0400

    secrets

--- simple_app.py
+++ simple_app.py
@@ -100,13 +100,13 @@ icons = {"assistant": "./logosmall.png", "user": "⛷️"}
 # Replicate Credentials
 with st.sidebar:
     st.title('CS GPT PRO! :)')
-    # if 'REPLICATE_API_TOKEN' in st.secrets:
-    #     replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    # else:
-    #     replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    #     if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-    #         st.warning('Please enter your Replicate API token.', icon='⚠️')
-    #         st.markdown("**Don't have an API token?** Head over to [Replicate](https://replicate.com) to sign up for one.")
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
+            st.warning('Please enter your Replicate API token.', icon='⚠️')
+            st.markdown("**Don't have an API token?** Head over to [Replicate](https://replicate.com) to sign up for one.")
 
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
     st.subheader("Adjust model parameters")
